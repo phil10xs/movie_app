a99a9c8836c23bc606c746d9a031a9449a0fd820
MAC <user@philicon.local>
2021-03-29 19:36:26 +0100

    Movies API Integration, Error Handling, Dio,  ReduX

--- lib/movies_api.dart
+++ lib/movies_api.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 class MovieAPI {
   static Future getMovies({String token, String url, Dio client}) async {
     final _client = client ?? Dio();
-    final url = 'https://movie-database-imdb-alternative.p.rapidapi.com';
+    final url =
+        'https://movie-database-imdb-alternative.p.rapidapi.com?i=tt4154796';
     final token = 'd373d5b188msh220ba54e19d2c80p10ac72jsnc96d766a4d8a';
 
     try {
